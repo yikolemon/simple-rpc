cc33d10d2543778990073cc7df9c18ffae65912e
zko0 <1142496307@qq.com>
2023-02-07 09:55:24 +0800

    2-7

---
+++ src/main/java/cn/zko0/zko0_Rpc/App.java
@@ -0,0 +1,13 @@
+package cn.zko0.zko0_Rpc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
